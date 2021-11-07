6bcee9d92cae3ff29d996e67f547ec3b972fcbe4
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-07 13:57:09 +0100

    Liczby KK

--- Program.cs
+++ Program.cs
@@ -4,8 +4,30 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void PrintUniqueNumbers(int[] numbersArray)
+        {
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (numbersArray[i] == numbersArray[j])
+                    {
+                        isDuplicate = true;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    Console.WriteLine(numbersArray[i]);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-        }
+            int[] numbers = { 2, 2, 1, 2, 5, 1, 2, 5, 1 };
+            PrintUniqueNumbers(numbers);
+        }    
     }
 }
