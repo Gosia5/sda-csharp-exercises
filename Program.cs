cec896f02a5d8e26aacffaefc60ed3bbc425bc40
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-07 12:27:03 +0100

    Silnia

--- Program.cs
+++ Program.cs
@@ -6,6 +6,21 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            int liczba = Convert.ToInt32(Console.ReadLine());
+        }
+        static int silnia(int liczba)
+        {
+            if(liczba < 0)
+            {
+                throw new ArgumentException("Błąd.");
+            }
+            int wynik = 1;
+            for (int i = liczba; i <= 1; i++)
+            {
+                wynik *= i;
+            }
+            return liczba * wynik;
         }
     }
 }
+   
