d10b5bc71b9b20dd45b8fb53b5524e0e62fd05d4
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-07 12:58:48 +0100

    Hasło

--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@ namespace sda_csharp_exercises
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Hasło(string[] args)
         {
+            string hasło = Console.ReadLine();
+            int długość = hasło.Length;
+            {
+                if(długość < 6)
+                { Console.WriteLine("Za mało liter."); }
+                if (długość = !char.IsUpperUpper)
+                { Console.WriteLine("Brak dużej litery."); }
+                if (długość = !int)
+                { Console.WriteLine("Brak cyfry."); }
+                if (!długość & *$#)
+                { Console.WriteLine("Brak znaku specjalnego"); }
+                else
+                { Console.WriteLine("Hasło poprawne"); }
+            }
         }
     }
 }
