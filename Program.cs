c8fc3e3f3dca1a7a4cf8635a3f13061d98a326d8
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-07 13:02:42 +0100

    Hasło poprawione

--- Program.cs
+++ Program.cs
@@ -4,22 +4,46 @@ namespace sda_csharp_exercises
 {
     class Program
     {
-        static void Hasło(string[] args)
+        static void Main(string[] args)
         {
-            string hasło = Console.ReadLine();
-            int długość = hasło.Length;
+            Console.WriteLine(HasRequiredComplexity("abc"));
+            Console.WriteLine(HasRequiredComplexity("aA$bbb1"));
+        }
+
+        private static bool HasRequiredComplexity(string password)
+        {
+            bool hasRequiredLength = password.Length >= 6;
+            bool containsLowerCharacter = false;
+            bool containsUpperCharacter = false;
+            bool containsDigit = false;
+            string specialCharacters = "&*$#";
+            bool containsSpecialCharacter = false;
+            foreach (char character in password.ToCharArray())
             {
-                if(długość < 6)
-                { Console.WriteLine("Za mało liter."); }
-                if (długość = !char.IsUpperUpper)
-                { Console.WriteLine("Brak dużej litery."); }
-                if (długość = !int)
-                { Console.WriteLine("Brak cyfry."); }
-                if (!długość & *$#)
-                { Console.WriteLine("Brak znaku specjalnego"); }
-                else
-                { Console.WriteLine("Hasło poprawne"); }
+                if (Char.IsLower(character))
+                {
+                    containsLowerCharacter = true;
+                }
+
+                if (Char.IsUpper(character))
+                {
+                    containsUpperCharacter = true;
+                }
+
+                if (Char.IsDigit(character))
+                {
+                    containsDigit = true;
+                }
+
+                if (specialCharacters.Contains(character))
+                {
+                    containsSpecialCharacter = true;
+                }
             }
+
+            return hasRequiredLength && containsLowerCharacter &&
+                   containsUpperCharacter && containsDigit &&
+                   containsSpecialCharacter;
         }
     }
 }
