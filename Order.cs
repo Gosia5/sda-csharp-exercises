3acd5258e42866c6a15ca4d15fe4e222ef50c5dc
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-20 15:04:30 +0100

    Opakowania

---
+++ Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Order
+    {
+        private List<OrderItem> items = new List<OrderItem>();
+
+        public void AddOrderItem(OrderItem item)
+        {
+            items.Add(item);
+        }
+
+        public decimal GetValue()
+        {
+            decimal total = 0;
+            foreach(OrderItem item in items)
+            {
+                item.GetValue();
+            }
+            return total;
+        }
+        public void DisplayOrderItem()
+        {
+            foreach (OrderItem item in items)
+            { Console.WriteLine(item); }
+            Console.WriteLine($"Wartość zamówienia = {GetValue()} PLN. ");
+        }
+    }
+}
