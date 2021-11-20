91befcf7eeea250c474b680bd186ff40694a8281
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-20 13:11:47 +0100

    Cat and Dog - Classes.

--- Program.cs
+++ Program.cs
@@ -1,40 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
     class Program
     {
-        public static int[] BubbleSort(int[] table)
-        {
-            for (int i = 0; i < table.Length - 1; i++)
-            {
-                for (int j = 0; j < table.Length - i - 1; j++)
-                {
-                    if (table[j] > table[j + 1])
-                    {
-                        Swap(ref table[j], ref table[j + 1]);
-
-                    }
-                }
-            }
-            return table;
-        }
-        public static void Swap(ref int x, ref int y)
-        {
-            int temp = 0;
-            temp = x;
-            x = y;
-            y = temp;
-        }
         static void Main(string[] args)
         {
-            int[] answer;
-            answer = BubbleSort(new int[] { 7, 1234, 1, 5, 23, 532, 6 });
-            foreach (int number in answer)
-            {
-                Console.WriteLine(number);
-            }
+            Cat cat = new Cat("Mruczek");
+            cat.MakeSound();
+            Dog dog = new Dog("Burek");
+            dog.MakeSound();
 
+            List<Animal> animals = new List<Animal>();
+            animals.Add(cat);
+            animals.Add(dog);
+
+            foreach(Animal a in animals)
+            {
+                a.MakeSound();
+                if(a is Cat)
+                {
+                    Console.WriteLine("It's a cat.");
+                }
+                else if(a is Dog)
+                {
+                    Console.WriteLine("It's a dog.");
+                }
+                else { Console.WriteLine("It's something."); }
+            }
         }
     }
 }
