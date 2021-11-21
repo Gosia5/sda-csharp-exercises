85c5d48b25ddef19a2117fc9bdc1196cfb1c911b
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-21 13:30:54 +0100

    Root sq W

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-           
+            MyPoint myPoint1 = new MyPoint(2, 3);
+            MyPoint myPoint2 = new MyPoint(5, 2);
+
+            LineSegment lineSegment1 = new LineSegment(myPoint1, myPoint2);
+            LineSegment lineSegment2 = new LineSegment(2, 3, 5, 2);
+
+            Console.WriteLine(lineSegment1.Lenght());
+
+            Console.WriteLine(lineSegment2.Lenght());
         }
     }
 }
