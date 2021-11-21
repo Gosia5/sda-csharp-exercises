e5842f6205163d8cdf7b25e64d624cd65de62ede
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-21 11:39:52 +0100

    Figure

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Circle : Figure
+    {
+        const double Pi = 3.14;
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {           
+            return Pi * radius * radius;
+        }
+    }
+}
