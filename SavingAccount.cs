7e10fdd1b7f2dc6cdf80b5133f335e215fe34250
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-21 09:45:04 +0100

    Suma kont

---
+++ SavingAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class SavingAccount : Account
+    {
+        public decimal Interest { get; private set; }
+
+        public void IncreaseInterest(decimal amount)
+        {
+            Interest = amount;
+        }
+
+        public SavingAccount(decimal balance = 0, decimal interest = 0):
+            base(balance)
+        {
+            Interest = interest;
+        }
+        public override decimal GetValue()
+        {
+            return Balance + Interest;
+        }
+    }
+}
