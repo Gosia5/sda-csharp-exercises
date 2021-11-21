7bcd4c525892d6a1ef2dfaad9129aae8fb05abc6
Malgorzata <malgorzata.swietlik1@gmail.com>
2021-11-21 12:57:19 +0100

    Gracz

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Player
+    {
+        public string Name { get; set; }
+        protected PlayerStats Stats { get; set; } = new PlayerStats();
+        public List<Item> Items { get; set; } = new List<Item>();
+
+        public int XP
+        {
+            get
+            {
+                return Stats.XP;
+            }
+
+            set
+            {
+                Stats.XP = value;
+            }
+        }
+    }
+}
